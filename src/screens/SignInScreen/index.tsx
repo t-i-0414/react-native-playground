5e44e0010e82339dde9cf5d7b514e43753834804
t-i-0414 <gutlo.ol0414@gmail.com>
2024-11-11 02:23:10 +0900

    feat: add SignInScreen

---
+++ src/screens/SignInScreen/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+export const SignInScreen = () => (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Text>Sign in</Text>
+  </View>
+);
