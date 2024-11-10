92ee50b04ab824b82c7b5e30c0aafe0ccc9b9f8a
t-i-0414 <gutlo.ol0414@gmail.com>
2024-11-11 00:12:34 +0900

    chore: update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,47 +1,63 @@
 import { ConfigContext, ExpoConfig } from 'expo/config';
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  name: 'anews-mobile',
-  slug: 'anews',
-  version: '1.0.0',
-  orientation: 'portrait',
-  icon: './src/assets/images/icon.png',
-  scheme: 'myapp',
-  userInterfaceStyle: 'automatic',
-  splash: {
-    image: './src/assets/images/splash.png',
-    resizeMode: 'contain',
-    backgroundColor: '#ffffff',
-  },
-  ios: {
-    supportsTablet: true,
-  },
-  android: {
-    adaptiveIcon: {
-      foregroundImage: './src/assets/images/icon.png',
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const baseConfig: ExpoConfig = {
+    name: 'anews-mobile',
+    slug: 'anews',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './src/assets/images/icon.png',
+    scheme: 'myapp',
+    userInterfaceStyle: 'automatic',
+    splash: {
+      image: './src/assets/images/splash.png',
+      resizeMode: 'contain',
       backgroundColor: '#ffffff',
     },
-  },
-  plugins: [
-    'expo-router',
-    [
-      'expo-build-properties',
-      {
-        android: {
-          enableProguardInReleaseBuilds: true,
-          enableShrinkResourcesInReleaseBuilds: true,
-        },
-      },
-    ],
-  ],
-  experiments: {
-    typedRoutes: true,
-  },
-  extra: {
-    eas: {
-      projectId: 'f2816330-e002-41bc-b897-bf1074b70334',
+    ios: {
+      supportsTablet: true,
     },
-    storybookEnabled: process.env.STORYBOOK_ENABLED,
-  },
-  owner: 'stockmark',
-});
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './src/assets/images/icon.png',
+        backgroundColor: '#ffffff',
+      },
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-build-properties',
+        {
+          android: {
+            enableProguardInReleaseBuilds: true,
+            enableShrinkResourcesInReleaseBuilds: true,
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      eas: {
+        projectId: 'f2816330-e002-41bc-b897-bf1074b70334',
+      },
+      storybookEnabled: process.env.STORYBOOK_ENABLED,
+    },
+    owner: 'stockmark',
+  };
+
+  return {
+    ...baseConfig,
+    ...(process.env.STORYBOOK_ENABLED && {
+      name: `${baseConfig.name}-storybook`,
+      slug: `${baseConfig.slug}-storybook`,
+      icon: './src/assets/images/icon-storybook.png',
+      splash: {
+        ...baseConfig.splash,
+        image: './src/assets/images/splash-storybook.png',
+        resizeMode: 'cover',
+      },
+    }),
+  };
+};
