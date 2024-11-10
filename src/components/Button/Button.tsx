a4bde3509530bd0f924d2bcadd3e1323a8dea905
t-i-0414 <gutlo.ol0414@gmail.com>
2024-11-11 02:37:21 +0900

    refactor: lint

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,17 +5,15 @@ export type MyButtonProps = {
   text: string;
 };
 
-export const MyButton = ({ onPress, text }: MyButtonProps) => {
-  return (
-    <TouchableOpacity
-      style={styles.container}
-      onPress={onPress}
-      activeOpacity={0.8}
-    >
-      <Text style={styles.text}>{text}</Text>
-    </TouchableOpacity>
-  );
-};
+export const MyButton = ({ onPress, text }: MyButtonProps) => (
+  <TouchableOpacity
+    style={styles.container}
+    onPress={onPress}
+    activeOpacity={0.8}
+  >
+    <Text style={styles.text}>{text}</Text>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
   container: {
