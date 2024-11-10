06c4f767252ce14485a0fda0d39cf070794b5f11
t-i-0414 <gutlo.ol0414@gmail.com>
2024-11-11 02:23:19 +0900

    feat: add HomeScreen

---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+export const HomeScreen = () => (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Text>Home</Text>
+  </View>
+);
